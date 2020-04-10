a311d97c7d7ef9dbe283eefed69b45346d222684
monguri <mongry@gmail.com>
2020-04-10 21:54:49 +0900

    コマンドリスト、コマンドキューを用意するところまで

--- Chapter03SutraCopy/main.cpp
+++ Chapter03SutraCopy/main.cpp
@@ -1,10 +1,14 @@
 #include <Windows.h>
 #include <tchar.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <vector>
 #ifdef _DEBUG
 #include <iostream>
 #endif
 
-using namespace std;
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 void DebugOutputFormatString(const char* format, ...)
 {
@@ -30,6 +34,13 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 const unsigned int window_width = 1280;
 const unsigned int window_height = 720;
 
+ID3D12Device* _dev = nullptr;
+IDXGIFactory6* _dxgiFactory = nullptr;
+ID3D12CommandAllocator* _cmdAllocator = nullptr;
+ID3D12GraphicsCommandList* _cmdList = nullptr;
+ID3D12CommandQueue* _cmdQueue = nullptr;
+IDXGISwapChain4* _swapchain = nullptr;
+
 #ifdef _DEBUG
 int main()
 #else
@@ -38,6 +49,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	DebugOutputFormatString("Show window test.");
 
+	// ウィンドウの生成
 	WNDCLASSEX w = {};
 	w.cbSize = sizeof(WNDCLASSEX);
 	w.lpfnWndProc = (WNDPROC)WindowProcedure;
@@ -62,6 +74,67 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		w.hInstance,
 		nullptr
 	);
+
+	// DXGIFactoryの生成
+	if (FAILED(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&_dxgiFactory))))
+	{
+		if (FAILED(CreateDXGIFactory2(0, IID_PPV_ARGS(&_dxgiFactory))))
+		{
+			return -1;
+		}
+	}
+
+	// NVIDIAアダプタの選択
+	std::vector<IDXGIAdapter*> adapters;
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0; _dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND; ++i)
+	{
+		adapters.push_back(tmpAdapter);
+	}
+
+	IDXGIAdapter* nvidiaAdapter = nullptr;
+	for (IDXGIAdapter* adapter : adapters)
+	{
+		DXGI_ADAPTER_DESC desc = {};
+		adapter->GetDesc(&desc);
+		std::wstring strDesc = desc.Description;
+		if (strDesc.find(L"NVIDIA") != std::string::npos)
+		{
+			nvidiaAdapter = adapter;
+			break;
+		}
+	}
+
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	// Direct3Dデバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+	for (D3D_FEATURE_LEVEL level : levels)
+	{
+		if (D3D12CreateDevice(nvidiaAdapter, level, IID_PPV_ARGS(&_dev)) == S_OK)
+		{
+			featureLevel = level;
+			break;
+		}
+	}
+
+	// コマンドアロケータとコマンドリストの生成
+	HRESULT result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_cmdAllocator));
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator, nullptr, IID_PPV_ARGS(&_cmdList));
+
+	// コマンドキューの生成
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	cmdQueueDesc.NodeMask = 0;
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
 
 	ShowWindow(hwnd, SW_SHOW);
 
