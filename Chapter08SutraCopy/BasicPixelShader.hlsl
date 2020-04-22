bf4aa2d3ae7f81a7f3a6b61c4ae22f1419b00686
monguri <mongry@gmail.com>
2020-04-22 22:30:16 +0900

    アンビエントの反映

--- Chapter08SutraCopy/BasicPixelShader.hlsl
+++ Chapter08SutraCopy/BasicPixelShader.hlsl
@@ -8,7 +8,7 @@ cbuffer Material : register(b1)
 {
 	float4 diffuse;
 	float4 specular;
-	float4 ambient;
+	float3 ambient;
 };
 
 float4 BasicPS(Output input) : SV_Target
@@ -16,5 +16,7 @@ float4 BasicPS(Output input) : SV_Target
 	float3 light = normalize(float3(1.0f, -1.0f, 1.0f));
 	float brightness = dot(-light, input.normal.xyz);
 	float2 sphereMapUV = input.vnormal.xy;
-	return float4(brightness, brightness, brightness, 1) * diffuse * tex.Sample(smp, input.uv) * sph.Sample(smp, sphereMapUV) + spa.Sample(smp, sphereMapUV);
+
+	float4 texColor = tex.Sample(smp, input.uv);
+	return float4(brightness, brightness, brightness, 1) * diffuse * texColor * sph.Sample(smp, sphereMapUV) + spa.Sample(smp, sphereMapUV) + float4(ambient * texColor.rgb, 1);
 }
