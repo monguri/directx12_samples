78bdaa5789a33b5d20739edfc687fed9eff1cae8
monguri <mongry@gmail.com>
2020-04-25 14:10:04 +0900

    main.cppに入っていた処理をApplication.cpp/hに移動した。 いくつかの関数に分けたのでComPtrがスコープからはずれて解放されないようにComPtrをApplicationクラス内に持たせたりしている。 現在、テクスチャの適用がおかしい不具合をかかえているが、とりあえず現状をコミット

--- Chapter09SutraCopy/Application.h
+++ Chapter09SutraCopy/Application.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <tchar.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+#include <vector>
+#include <map>
+#include <d3dcompiler.h>
+#include <DirectXTex.h>
+#include <d3dx12.h>
+#include <wrl.h>
+
 class Application
 {
 private:
@@ -13,5 +24,70 @@ public:
 	bool Init();
 	void Run();
 	void Terminate();
+
+private:
+	WNDCLASSEX _windowClass;
+	HWND _hwnd;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> _depthBuffer = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> _vertBuff = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> _idxBuff = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> _materialBuff = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> _constBuff = nullptr;
+
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> _textureResources;
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> _sphResources;
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> _spaResources;
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> _toonResources;
+
+	DirectX::XMMATRIX _worldMat;
+
+	// シェーダに渡すために必要なものだけ選択したデータ
+	struct MaterialForHlsl
+	{
+		DirectX::XMFLOAT3 diffuse;
+		float alpha;
+		DirectX::XMFLOAT3 specular;
+		float specularity;
+		DirectX::XMFLOAT3 ambient;
+	};
+
+	// PMDMaterialのうち、MaterialForHlsl以外のマテリアル情報をもっておく
+	// ためのデータ
+	struct AdditionalMaterial
+	{
+		std::string texPath;
+		int toonIdx;
+		bool edgeFlg;
+	};
+
+	// MaterialForHlslとAdditionalMaterialをまとめたもの
+	struct Material
+	{
+		unsigned int indicesNum;
+		MaterialForHlsl material;
+		AdditionalMaterial additional;
+	};
+
+	std::vector<Material> _materials;
+
+	struct SceneData* _mapScene = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> _basicDescHeap = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> _materialDescHeap = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12Fence> _fence = nullptr;
+	UINT _fenceVal = 0;
+
+	D3D12_VERTEX_BUFFER_VIEW _vbView;
+	D3D12_INDEX_BUFFER_VIEW _ibView;
+
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> _pipelinestate = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> _rootsignature = nullptr;
+
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> _backBuffers;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> _rtvHeaps = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> _dsvHeap = nullptr;
 };
 
